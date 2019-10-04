4bb9d07e30cbc31d63439de2623f0ae336142445
Matthew O <deltas2k@gmail.com>
2019-10-03 18:48:13 -0600

    model controller  commit

--- starwarsAPI/starwarsAPI/controllers/model controllers/StarshipController.swift
+++ starwarsAPI/starwarsAPI/controllers/model controllers/StarshipController.swift
@@ -9,5 +9,33 @@
 import Foundation
 
 class StarshipController {
+    let baseURL = URL(string: StarshipConstants.baseURL)
+    
+    func fetchStarships(with searchText: String, completion: @escaping ([Starship]) -> Void) {
+        guard let url = baseURL else {completion([]); return}
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        let queryItem = URLQueryItem(name: StarshipConstants.keyItem, value: searchText)
+        
+        urlComponents?.queryItems = [queryItem]
+        
+        guard let finalURL = urlComponents?.url else {completion([]);return}
+        
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion([]); return
+            }
+            guard let data = data else {completion([]);return}
+            do {
+                let decoded = try JSONDecoder().decode(TopLevelDict.self, from: data)
+                completion(decoded.starships)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+        .resume()
+        
+    }
     
 }
